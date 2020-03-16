473a34dd99c1f45e4bd6d0807aaac6fb7c108f4f
pavaniguttapalli <60918345+pavaniguttapalli@users.noreply.github.com>
2020-03-16 16:55:10 +0530

    Adding Driver class
    Commit to master

---
+++ GitHub_Jenkins/GitHub_Jenkins/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+
+namespace GitHub_Jenkins
+{
+    public class Class1
+    {
+        public IWebDriver driver;   
+        public void Method1()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://accounts.google.com/signup/v2/webcreateaccount?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2Femail&gmb=exp&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp";
+        }
+    }
+}
